5e4c479fc889d820b904b7e13cd5cc36251ca868
Ibuki-Y <ibu.love.drum@gmail.com>
2022-05-22 21:15:42 +0900

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,91 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <GlobalHeader @delete-all="deleteAll" />
+    <v-main>
+      <v-container>
+        <router-view :books="books" @add-book-list="addBook" @update-book-info="updateBookInfo" />
+      </v-container>
+    </v-main>
+    <!-- <GolobalFooter /> -->
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import GlobalHeader from "@/global/GlobalHeader.vue";
+// import GolobalFooter from "@/global/GlobalFooter.vue";
 
-nav {
-  padding: 30px;
-}
+const STORAGE_KEY = "books";
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+export default {
+  name: "App",
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  components: {
+    GlobalHeader,
+    // GolobalFooter,
+  },
+
+  data() {
+    return {
+      books: [],
+      newBook: null,
+    };
+  },
+  mounted() {
+    if (localStorage.getItem(STORAGE_KEY)) {
+      try {
+        this.books = JSON.parse(localStorage.getItem(STORAGE_KEY));
+      } catch (e) {
+        localStorage.removeItem(STORAGE_KEY);
+      }
+    }
+  },
+  methods: {
+    addBook(e) {
+      this.books.push({
+        id: this.books.length,
+        title: e.title,
+        image: e.image,
+        description: e.description,
+        readDate: "",
+        memo: "",
+      });
+      // this.newBook = "";
+      this.saveBooks();
+      this.goToEditPage(this.books.slice(-1)[0].id); // slice(-1)[0]: 最後に追加したデータ
+    },
+    removeBook(x) {
+      this.books.splice(x, 1);
+      this.saveBooks();
+    },
+    saveBooks() {
+      const parsed = JSON.stringify(this.books);
+      localStorage.setItem(STORAGE_KEY, parsed);
+    },
+    updateBookInfo(e) {
+      const updateInfo = {
+        id: e.id,
+        readDate: e.readDate,
+        memo: e.memo,
+        title: this.books[e.id].title,
+        image: this.books[e.id].image,
+        description: this.books[e.id].description,
+      };
+      this.books.splice(e.id, 1, updateInfo);
+      this.saveBooks();
+      this.$router.push("/");
+    },
+    goToEditPage(id) {
+      this.$router.push(`/edit/${id}`);
+    },
+    deleteAll() {
+      const isDeleted = "really?";
+      if (window.confirm(isDeleted)) {
+        localStorage.setItem(STORAGE_KEY, "");
+        localStorage.removeItem(STORAGE_KEY);
+        this.books = [];
+        window.location.reload();
+      }
+    },
+  },
+};
+</script>
---
+++ src/global/GlobalHeader.vue
@@ -0,0 +1,22 @@
+import GlobalHeader from '@/global/GlobalHeader.vue';
+<template>
+  <div>
+    <v-app-bar app color="primary" dark>
+      <v-spacer></v-spacer>
+      <v-btn color="error" @click="deleteAll">削除</v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'GlobalHeader',
+  methods:{
+    deleteAll(){
+      this.$emit('delete-all')
+    }
+  }
+};
+</script>
+
+<style></style>
